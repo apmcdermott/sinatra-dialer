2bcdf7dc54f0ab21bc1d491c1f5456c551f0b7c2
Alex Bromley <abromley@patientslikeme.com>
2016-06-10 10:30:44 -0400

    Attach a giphy randomly selected from results

--- sinatra-dialer.rb
+++ sinatra-dialer.rb
@@ -6,6 +6,25 @@ end
 
 post '/' do
 
+  search_term = params['search_term']
+
+  uri = URI.parse("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=dc6zaTOxFJmzC")
+
+  response = Net::HTTP.get_response(uri)
+
+  parsed_response = JSON.parse(response.body)
+
+  number_of_results = parsed_response["pagination"]["count"].to_i
+
+  results = parsed_response["data"]
+
+  #Select randomly from resuls
+  number = Random.new.rand(number_of_results).to_i
+
+  selected_result = results[number]
+
+  @image_url = selected_result["images"]["fixed_height"]["url"]
+
   # put your own credentials here 
   account_sid = 'AC0cdeecfe4679db365c25a166f5ebfcf9' 
   auth_token = '5c9219d5798ed86241acbd198bb0331b' 
@@ -13,12 +32,13 @@ post '/' do
   # set up a client to talk to the Twilio REST API 
   @client = Twilio::REST::Client.new account_sid, auth_token 
    
-  @client.account.messages.create({
+  @message_result = @client.account.messages.create({
     :from => '+16179350712', 
     :to => '6179971870', 
-    :body => 'hello',  
+    :body => "You searched for " + search_term,
+    :media_url => @image_url
   })
-  
+
   erb :create
 
 end
