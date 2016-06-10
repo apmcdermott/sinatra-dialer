69a8d54b13a4be090371e8fde0ab6baedf87a236
Alex Bromley <abromley@patientslikeme.com>
2016-06-10 08:33:41 -0400

    send message on post

--- sinatra-dialer.rb
+++ sinatra-dialer.rb
@@ -1,5 +1,11 @@
 get '/' do
 
+  erb :index
+  
+end
+
+post '/' do
+
   # put your own credentials here 
   account_sid = 'AC0cdeecfe4679db365c25a166f5ebfcf9' 
   auth_token = '5c9219d5798ed86241acbd198bb0331b' 
@@ -11,6 +17,8 @@ get '/' do
     :from => '+16179350712', 
     :to => '6179971870', 
     :body => 'hello',  
-  }) 
+  })
   
+  erb :create
+
 end
