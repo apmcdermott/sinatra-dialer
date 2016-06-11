14e549285eaa935415cd19c3b12c2473ab967c27
Alex Bromley <abromley@patientslikeme.com>
2016-06-11 16:58:24 -0400

    Make cell number dynamic entry.  Handle zero results.

--- sinatra-dialer.rb
+++ sinatra-dialer.rb
@@ -6,6 +6,7 @@ end
 
 post '/' do
 
+  cell_number = params['cell_number']
   search_term = params['search_term']
 
   uri = URI.parse("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=dc6zaTOxFJmzC")
@@ -18,27 +19,33 @@ post '/' do
 
   results = parsed_response["data"]
 
-  #Select randomly from resuls
-  number = Random.new.rand(number_of_results).to_i
+  #Select randomly from results
+  if number_of_results == 0
+    erb :no_results
+  else
 
-  selected_result = results[number]
+    number = Random.new.rand(number_of_results).to_i
 
-  @image_url = selected_result["images"]["fixed_height"]["url"]
+    selected_result = results[number]
 
-  # put your own credentials here 
-  account_sid = 'AC0cdeecfe4679db365c25a166f5ebfcf9' 
-  auth_token = '5c9219d5798ed86241acbd198bb0331b' 
-   
-  # set up a client to talk to the Twilio REST API 
-  @client = Twilio::REST::Client.new account_sid, auth_token 
-   
-  @message_result = @client.account.messages.create({
-    :from => '+16179350712', 
-    :to => '6179971870', 
-    :body => "You searched for " + search_term,
-    :media_url => @image_url
-  })
+    @image_url = selected_result["images"]["fixed_height"]["url"]
 
-  erb :create
+    # put your own credentials here 
+    account_sid = 'AC0cdeecfe4679db365c25a166f5ebfcf9' 
+    auth_token = '5c9219d5798ed86241acbd198bb0331b' 
+     
+    # set up a client to talk to the Twilio REST API 
+    @client = Twilio::REST::Client.new account_sid, auth_token 
+     
+    @message_result = @client.account.messages.create({
+      :from => '+16179350712', 
+      :to => cell_number, 
+      :body => "You searched for " + search_term,
+      :media_url => @image_url
+    })
+
+    erb :create
+
+  end
 
 end
