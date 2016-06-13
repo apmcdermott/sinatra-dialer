9a78ba1df8c888492e246313d8351ead761122f3
Alex Bromley <abromley@patientslikeme.com>
2016-06-13 09:12:07 -0400

    use twilio number to receive tests

--- test/sinatra-dialer-test.rb
+++ test/sinatra-dialer-test.rb
@@ -8,6 +8,10 @@ class SinatraDialerTest < MiniTest::Unit::TestCase
     Sinatra::Application
   end
 
+  def receiving_number
+     "6179350712"
+  end
+
   def test_it_loads_search_page
     get '/'
     assert last_response.ok?
@@ -15,19 +19,19 @@ class SinatraDialerTest < MiniTest::Unit::TestCase
   end
 
   def test_it_handles_blank_values
-    post '/', :search_term => 'Simon', :cell_number => '6179971870'
+    post '/', :search_term => 'Simon', :cell_number => receiving_number
     assert last_response.ok?
     last_response.body.include?('No results found.')
   end
 
   def test_it_retrieves_image_results
-    post '/', :search_term => 'Simon', :cell_number => '6179971870'
+    post '/', :search_term => 'Simon', :cell_number => receiving_number
     assert last_response.ok?
     last_response.body.include?('Image URL used: http:')
   end
 
   def test_it_sends_a_message
-    post '/', :search_term => 'Simon', :cell_number => '6179971870'
+    post '/', :search_term => 'Simon', :cell_number => receiving_number
     assert last_response.body.include?('Message Sending Result Status: queued')
   end
 
