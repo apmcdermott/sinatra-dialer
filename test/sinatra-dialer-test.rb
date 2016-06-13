068aafcc82d7c67c05789ba07e678bf0baa3defb
Alex Bromley <abromley@patientslikeme.com>
2016-06-13 08:34:32 -0400

    Update tests to load config.  Add view for nil results.

--- test/sinatra-dialer-test.rb
+++ test/sinatra-dialer-test.rb
@@ -1,7 +1,7 @@
 require File.expand_path '../test_helper.rb', __FILE__
 
 class SinatraDialerTest < MiniTest::Unit::TestCase
-  
+
   include Rack::Test::Methods
 
   def app
@@ -14,15 +14,21 @@ class SinatraDialerTest < MiniTest::Unit::TestCase
     last_response.body.include?('Image Search Term')
   end
 
-  def test_it_retrieves_image_results
+  def test_it_handles_blank_values
     get '/'
     assert last_response.ok?
-    assert_equal 'Hello World', last_response.body
+    last_response.body.include?('Image Search Term')
+  end
+
+  def test_it_retrieves_image_results
+    post '/', :search_term => 'Simon'
+    assert last_response.ok?
+    last_response.body.include?('Image URL used: http:')
   end
 
   def test_it_sends_a_message
-    get '/', :name => 'Simon'
-    assert last_response.body.include?('Simon')
+    post '/', :search_term => 'Simon'
+    assert last_response.body.include?('Message Sending Result Status: queued')
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,8 @@ ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'rack/test'
 
+  Rack::Builder.new do
+    eval File.read('config.ru')
+  end
+
 require File.expand_path '../../sinatra-dialer.rb', __FILE__
